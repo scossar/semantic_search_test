965c9214ed313404a1258a47d7f0b264a686c04e
scossar <scossar3@gmail.com>
2025-06-22 02:41:23 +0000

    remove notes_dir argument

--- obsidian_semantic_search.py
+++ obsidian_semantic_search.py
@@ -1,39 +1,16 @@
 from pathlib import Path
-from chromadb.api.types import normalize_embeddings
 from sentence_transformers import SentenceTransformer
-import numpy as np
 import frontmatter
-from typing import Dict, List, Set, TypedDict
+from typing import Dict, List, Set
 import typer
 from rich import print
 import chromadb
 from chromadb.config import Settings
 
-# notes:
-# - generate_vector_embeddings_for_local_files.md
-# - python_typing_library.md
-# - generating_embeddings_from_markdown_posts.md
-# - getting_started_with_chromadb.md
-
-# class ChunkMetadata(TypedDict):
-#     start_line: int
-#     end_line: int
-#     text: str
-#
-# class DocumentMetadata(TypedDict):
-#     title: str
-#     chunks: List[ChunkMetadata]
-#
-# class ChunkEmbedding(TypedDict):
-#     start_line: int
-#     end_line: int
-#     embedding: np.ndarray
-
 
 class SemanticSearch:
-    def __init__(self, notes_dir: str, model_name: str = "all-mpnet-base-v2",
+    def __init__(self, model_name: str = "all-mpnet-base-v2",
                  persist_directory: str = "./chroma_db"):
-        self.notes_dir = Path(notes_dir)
         self.skip_dirs: Set[str] = {
             "node_modules",
             ".git",
@@ -45,18 +22,15 @@ class SemanticSearch:
         self.model = SentenceTransformer(model_name)
         self.chroma_client = chromadb.PersistentClient(
             path=persist_directory,
-            settings=Settings(anonymized_telemetry=False)  # chroma collects usage info: set to True to disable
+            settings=Settings(anonymized_telemetry=False)  # chroma collects usage info (?)
         )
 
         # no embedding function is supplied, so chroma will use sentence_transformer
         self.collection = self.chroma_client.get_or_create_collection(
             name="markdown_chunks",
-            # I think these are the default values, the Chroma docs aren't great. It seems that metadata can be# used for configuration.
+            # I think these are the default values, the Chroma docs aren't great. It seems that metadata can be used for configuration.
             metadata={"hnsw:space": "cosine"}
         )
-        # self.embeddings: Dict[str, List[ChunkEmbedding]] = {}
-        # self.metadata: Dict[str, DocumentMetadata] = {}
-
 
     def sliding_window_chunks(self, content: str, window_lines: int = 10, stride: int = 5):
         lines = content.split('\n')
@@ -74,29 +48,26 @@ class SemanticSearch:
 
         return chunks
 
-    def index_documents(self, force_all=False, file_path=None):
+    def index_documents(self, notes_dir: str, force_all=False):
         """
         Index documents with update detection
 
         Args:
-            force_all: if True, re-index everything
-            file_path: if provided, only index the specific file
+            notes_dir: the directory to index
+            force_all: if True, re-index everything (not yet implemented)
         """
-        if file_path:
-            self._index_file(Path(file_path))
-            print(f"indexed [bold green]{file_path}[/bold green]\n")
-        else:
-            for path in self.notes_dir.rglob("*"):
-                if not force_all:
-                    if not self._should_process_file(path):
-                        continue
-                    if self._is_up_to_date(path):
-                        print(f"skipping indexing for [bold green]{path}[/bold green]\nFile up to date")
-                        continue
-                self._index_file(path)
-                print(f"indexed [bold green]{path}[/bold green]\n")
 
-    def _index_file(self, filepath: Path):
+        for path in Path(notes_dir).rglob("*"):
+            if not force_all:
+                if not self._should_process_file(path):
+                    continue
+                if self._is_up_to_date(path):
+                    print(f"Skipping indexing for [bold green]{path}[/bold green]\nFile up to date")
+                    continue
+            self.index_file(path)
+            print(f"Indexed [bold green]{path}[/bold green]\n")
+
+    def index_file(self, filepath: Path):
         """Index a single document"""
 
         post = frontmatter.load(str(filepath))
@@ -148,7 +119,7 @@ class SemanticSearch:
 
         if not isinstance(file_id, str):
             return False  # probably need a different check here to handle case of no file_id frontmatter
-        
+
         file_mtime = filepath.stat().st_mtime
         existing = self.collection.get(
             where={"file_id": file_id},
@@ -167,7 +138,8 @@ class SemanticSearch:
         if not isinstance(indexed_at, (int, float)):
             return False  # invalid timestamp
 
-        return file_mtime <= indexed_at
+        # add 1 second tolerance to deal with rounding issues
+        return file_mtime <= indexed_at + 1.0
 
     def _should_process_file(self, filepath: Path) -> bool:
         if any(part.startswith(".") for part in filepath.parts):
@@ -208,10 +180,10 @@ class SemanticSearch:
 
         return formatted_results
 
-    def find_files_by_ids(self, file_ids: List[str]) -> Dict[str, str]:
+    def find_files_by_ids(self, notes_dir: str,  file_ids: List[str]) -> Dict[str, str]:
         results = {}
 
-        for filepath in self.notes_dir.rglob("*"):
+        for filepath in Path(notes_dir).rglob("*"):
             if not self._should_process_file(filepath):
                 continue
 
@@ -228,19 +200,20 @@ app = typer.Typer()
 @app.command()
 def index_directory(notes_dir: str):
     """Index all markdown files in a directory"""
-    semantic_search = SemanticSearch(notes_dir=notes_dir)
-    semantic_search.index_documents()
+    semantic_search = SemanticSearch()
+    semantic_search.index_documents(notes_dir)
 
 @app.command()
-def index_file(notes_dir: str, file_path: str):
+def index_file(file_path: Path):
     """Index all markdown files in a directory"""
-    semantic_search = SemanticSearch(notes_dir=notes_dir)
-    semantic_search.index_documents(file_path=file_path)
+    semantic_search = SemanticSearch()
+    file_path = Path(file_path)
+    semantic_search.index_file(filepath=file_path)
 
 @app.command()
-def search(notes_dir: str, query: str):
+def search(query: str):
     """Search indexed documents"""
-    semantic_search = SemanticSearch(notes_dir=notes_dir)  # the notes_dir isn't used to limit the scope of the search
+    semantic_search = SemanticSearch()
     results = semantic_search.search(query)
 
     print("\n")
@@ -253,8 +226,7 @@ def search(notes_dir: str, query: str):
         start_line = metadata["start_line"]
         end_line = metadata["end_line"]
 
-        print(f"[bold green]{title}[/bold green]\nsimilarity: {similarity}\nstart_line: {start_line}\n{content}\n\n")
-
+        print(f"[bold green]{title}[/bold green]\nsimilarity: {similarity}\nlines: {start_line}-{end_line}\n{content}\n\n")
 
 if __name__ == "__main__":
     app()
